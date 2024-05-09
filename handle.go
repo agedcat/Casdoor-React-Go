d8d805d586425f1a26bc9bd783e5b1bafa05c9fd
agedcat <2324261907@qq.com>
2024-05-09 14:29:00 +0800

    init repo

--- handle.go
+++ handle.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
 	"github.com/gin-gonic/gin"
@@ -28,33 +27,6 @@ func signinHandler(c *gin.Context) {
 	}
 
 	fmt.Println(responsedata)
-
-	c.JSON(http.StatusOK, responsedata)
-}
-
-func getUserinfo(c *gin.Context) {
-	authHeader := c.GetHeader("Authorization")
-	if authHeader == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "authHeader is empty"})
-	}
-
-	token := strings.Split(authHeader, "Bearer ")
-	if len(token) != 2 {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "token is not valid Bearer token"})
-	}
-
-	claims, err := casdoorsdk.ParseJwtToken(token[1])
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to parse JWT token"})
-	}
-
-	c.Header("Content-Type", "application/json")
-	c.Writer.WriteHeader(http.StatusOK)
-
-	responsedata := map[string]interface{}{
-		"status": "ok",
-		"data":   claims.User,
-	}
 
 	c.JSON(http.StatusOK, responsedata)
 }
